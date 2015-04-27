70c73130683e70201d559e3b5c32e992353e51a3
Ian MacDonald & Kevin Chen <student@epicodus.com>
2015-04-27 10:07:06 -0700

    Add merge method to OurHash class.

--- lib/our_hash.rb
+++ lib/our_hash.rb
@@ -5,6 +5,18 @@ class OurHash
     @hash = [@keys, @values]
   end
 
+  define_method(:keys) do
+    keys = @keys
+  end
+
+  define_method(:values) do
+    values = @values
+  end
+
+  define_method(:hash) do
+    hash = @hash
+  end
+
   define_method(:fetch) do |key|
     value = ""
     @keys.each_index() do |key_index|
@@ -22,5 +34,25 @@ class OurHash
 
   define_method(:has_key?) do |key|
     @keys.include?(key)
+  end
+
+  define_method(:length) do
+    @keys.length()
+  end
+
+  define_method(:merge) do |array|
+    other_keys = array.keys()
+    other_values = array.values()
+    new_keys = @keys
+    new_values = @values
+    other_keys.each_index() do |key_index|
+      new_keys.push(other_keys[key_index])
+      new_values.push(other_values[key_index])
+    end
+    new_hash = OurHash.new()
+    new_keys.each_index() do |key_index|
+      new_hash.store(new_keys[key_index], new_values[key_index])
+    end
+    new_hash.hash()
   end
 end
--- spec/our_hash_spec.rb
+++ spec/our_hash_spec.rb
@@ -16,4 +16,20 @@ describe(OurHash) do
      expect(test_hash.has_key?("Illiad")).to(eq(true))
    end
   end
+  describe('length') do
+    it('returns the length of the hash') do
+      test_hash = OurHash.new()
+      test_hash.store("Illiad", "Homer")
+      expect(test_hash.length()).to(eq(1))
+    end
+  end
+  describe('#merge') do
+    it('merges two hashes, and returns a hash of the merge') do
+      test_hash = OurHash.new()
+      test_hash.store("Illiad", "Homer")
+      other_hash = OurHash.new()
+      other_hash.store("kitten", "cute")
+      expect(test_hash.merge(other_hash)).to(eq([["Illiad", "kitten"],["Homer", "cute"]]))
+    end
+  end
 end
